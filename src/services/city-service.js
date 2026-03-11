f50e16f6edc95db529f0baabf1fc5cdfa7e126e8
shekharsingh <shekharsingh36912@gmail.com>
2026-03-11 17:17:38 +0530

    Adfded city service

---
+++ src/services/city-service.js
@@ -0,0 +1,47 @@
+const { CityRepository } = require('../repository/index');
+
+class CityService{
+    constructor() {
+        this.cityRepository = new CityRepository();
+    }
+
+    async createCity(data) {
+        try {
+            const city = await this.cityRepository.createCity(data);
+            return city;
+        } catch (error) {
+            console.log('something went wrong');
+            throw { error }
+        }
+    }
+
+    async deleteCity(cityId) {
+        try {
+            const response = this.cityRepository.deleteCity(cityId);
+            return response;
+        } catch (error) {
+            console.log('something went wrong');
+            throw { error }
+        }
+    }
+
+    async updateCity(cityId) {
+        try {
+            const city = await this.cityRepository.UpdateCity(cityId,data);
+            return city;
+        } catch (error) {
+            console.log('something went wrong');
+            throw { error }
+        }
+    }
+
+    async getCity(cityId){
+        try {
+            const city = await this.cityRepository.GetCity(cityId);
+            return city;
+        } catch (error) {
+            console.log('something went wrong');
+            throw { error }
+        }
+    }
+}
